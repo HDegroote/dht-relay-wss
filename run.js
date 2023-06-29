e3f20f1cebb7d8d96b3a394fb5fd493c337242a2
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-06-29 02:30:52 +0200

    Bugfix int config vars (#3)

--- run.js
+++ run.js
@@ -7,13 +7,13 @@ const setup = require('./index')
 
 function loadConfig () {
   return {
-    wsPort: process.env.WS_PORT || 8080,
-    dhtPort: process.env.DHT_PORT,
+    wsPort: parseInt(process.env.WS_PORT || 8080),
+    dhtPort: parseInt(process.env.DHT_PORT || 0),
     logLevel: process.env.LOG_LEVEL || 'info',
     host: process.env.HOST || '127.0.0.1',
     sShutdownMargin: process.env.S_SHUTDOWN_MARGIN == null
       ? 10
-      : process.env.S_SHUTDOWN_MARGIN
+      : parseInt(process.env.S_SHUTDOWN_MARGIN)
   }
 }
 
