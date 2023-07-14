69b48a40a8b887b63cb78671a219aae09257d16e
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-07-15 00:31:53 +0200

    Fix typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DHT = require('@hyperdht')
+const DHT = require('hyperdht')
 const { relay } = require('@hyperswarm/dht-relay')
 const Stream = require('@hyperswarm/dht-relay/ws')
 const fastify = require('fastify')
